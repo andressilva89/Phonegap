4b73d16cf4e80bbea3b3ad41a85e8482b86b60d0
Andre Filipi <andrefilipigomes@yahoo.com.br>
2016-11-23 20:56:45 -0200

    321

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,54 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function pronto(page) {
+  var funcao = window['page' + page];
+  document.addEventListener('deviceready', funcao, false);
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function page1() {
+  window.addEventListener("batterystatus", onBatteryStatus, false);
 
-        console.log('Received Event: ' + id);
+
+}
+
+function onBatteryStatus(status) {
+  document.getElementById('battery').innerHTML =status.level;
+
+  navigator.notification.alert(
+    "Nivel da Bateria: " + status.level,
+    nada, "Bateria");
+
+  console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+}
+
+
+function page2() {
+  document.getElementById('model').innerHTML = device.model;
+  document.getElementById('version').innerHTML = device.cordova;
+
+  document.getElementById('doubletouch').addEventListener('touchend', function (evt) {
+    if(evt.touchesList.length == 2){
+      navigator.vibrate(200);
     }
-};
+  });
+}
+
+function page3() {
+  obter_Celerometro();
+  document.getElementById('Refresh').addEventListener('click',function () {
+    obter_Celerometro();
+  });
+
+
+}
+
+function obter_Celerometro() {
+  navigator.accelerometer.getCurrentAcceleration(function (celerometro) {
+    document.getElementById('CeleratorX').innerHTML = celerometro.x;
+    document.getElementById('CeleratorY').innerHTML = celerometro.y;
+    document.getElementById('CeleratorZ').innerHTML = celerometro.z;
+    document.getElementById('CeleratorTS').innerHTML = celerometro.timestamp;
+  });
+}
+
+function nada() {
+  
+}
